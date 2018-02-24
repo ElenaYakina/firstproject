a152a2787122beba1b38d3edddd054ab35830240
ElenaYakina <ms.elena.yakina@gmail.com>
2018-02-25 01:41:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <style src="index.css"></style>
+  <title>Document</title>
+</head>
+<body>
+
+  <!--   git-scm.com/book/ru/v1 -->
+
+  <h1>Hello</h1>
+
+  <div class="container">
+    <div class="display"></div>
+    <div class="btns">
+      <button class="btn-item">01</button>
+      <button class="btn-item">02</button>
+      <button class="btn-item">03</button>
+      <button class="btn-item">04</button>
+      <button class="btn-item">05</button>
+      <button class="btn-item">06</button>
+      <button class="btn-item">07</button>
+      <button class="btn-item">08</button>
+      <button class="btn-item">09</button>
+      <button class="btn-item">10</button>
+      <button class="btn-item">+</button>
+      <button class="btn-item">-</button>
+      <button class="btn-item">*</button>
+      <button class="btn-item">/</button>
+      <button class="btn-item">=</button>
+
+    </div>
+  </div>
+
+
+</body>
+</html>
